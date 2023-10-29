7b454cc98d8d8bf33372eb32053424329f7e79d9
Nikolay Lositskiy <nikolay@uscreen.tv>
2023-10-29 16:46:16 +0100

    add delivery setup class

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -5,6 +5,7 @@ class PaymentsController < ApplicationController
     purchase_result = Billing::Purchase.new(user: current_user, product: @product).make_purchase
 
     if purchase_result.success?
+      setup_delivery
       redirect_to :successful_payment_path
     else
       redirect_to :failed_payment_path, note: purchase_result.error_message
@@ -15,5 +16,9 @@ class PaymentsController < ApplicationController
 
   def find_product
     @product = Product.find(params[:product_id])
+  end
+
+  def setup_delivery
+    SetupDeliveryWorker.perform_async(user_id: current_user.id, weight: @product.weight)
   end
 end
